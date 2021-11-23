34e4381e1a49d1ab7cc1cc1482eb0e1f1804b9ea
Ian Sanders <iansan5653@gmail.com>
2021-11-23 21:14:35 +0000

    Add basic theme and app styles

---
+++ src/components/App/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./styles.css"
+
+function App(): React.ReactElement {
+  return (
+    <div className="App">
+      <div className="App-content">
+        <header>
+          <h1 className="App-title">Screw Calculator</h1>
+        </header>
+      Hello, world. ...
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "the-new-css-reset/css/reset.css"
 import './index.css';
-import App from './App';
+import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
